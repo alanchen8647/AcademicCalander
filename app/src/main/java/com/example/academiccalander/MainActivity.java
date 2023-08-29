26a9ba7339d963f754a30f94d8ba9a035b2bc280
Alan chen <alanchen8647@gmail.com>
2023-08-29 19:14:10 -0400

    Sign in page created set up firebase & firebase repository now google services is avaliable

--- app/src/main/java/com/example/academiccalander/MainActivity.java
+++ app/src/main/java/com/example/academiccalander/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.academiccalander;
 
+import android.content.Intent;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+
+        }
     }
 }
